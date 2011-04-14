1a3a703b5cecb3ac5409841e26ea5fe3a02f7115
ant512 <devnull@localhost>
2011-04-14 09:25:34 +0100

    LevelBase iterates over blocks in the correct order again when gravity is inverted.

--- src/levelbase.cpp
+++ src/levelbase.cpp
@@ -112,17 +112,32 @@ void LevelBase::iterateBlocks(bool isGravityInverted) {
 	s32 stop = 0;
 	s32 increment = 1;
 
-	//if (isGravityInverted) {
+	if (!isGravityInverted) {
 		stop = _width * _height;
-	//} else {
-	//	start = (_width * _height) - 1;
-	//	stop = -1;
-	//	increment = -1;
-	//}
+	} else {
+		start = (_width * _height) - 1;
+		stop = -1;
+		increment = -1;
+	}
 
 	BlockBase* block = NULL;
 
-	// Iterate over all blocks and call their own iterate() methods
+	// Iterate over all blocks.  This algorithm looks badly designed because
+	// it iterates over all blocks 4 times.  However, this is necessary to
+	// replicate the behaviour of the original game.  If the behaviours do not
+	// match, the levels will not work as expected.  The algorithm will:
+	//
+	// - Slide all blocks left that are capable of doing so.
+	// - Slide all blocks right that are capable of doing so.
+	// - Cause any blocks that can fall to fall.
+	// - Run the blocks' own iterate() methods to handle anything else that
+	//   needs to happen to them.
+	//
+	// Each block will only perform one movement (slide left, right or fall)
+	// per iteration of the game, so there is no problem in calling a block's
+	// slideLeft() method then calling its slideRight() method immediately
+	// afterwards - the second call won't cause it to slide back to its original
+	// position.
 	for (s32 i = start; i != stop; i += increment) {	
 		block = _data[i];
 		if (block == NULL) continue;
