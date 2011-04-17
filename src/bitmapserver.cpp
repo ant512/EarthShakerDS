731317f36112c7404c79d3b8adc09ec0d560503d
ant512 <devnull@localhost>
2011-04-17 10:49:22 +0100

    Fixed bitmapserver.cpp.

--- src/bitmapserver.cpp
+++ src/bitmapserver.cpp
@@ -8,10 +8,18 @@ WoopsiGfx::Bitmap* BitmapServer::_boulderExplodeBmp4 = NULL;
 WoopsiGfx::Bitmap* BitmapServer::_boulderExplodeBmp5 = NULL;
 WoopsiGfx::Bitmap* BitmapServer::_boulderExplodeBmp6 = NULL;
 WoopsiGfx::Bitmap* BitmapServer::_boulderExplodeBmp7 = NULL;
+
+WoopsiGfx::Bitmap* BitmapServer::_brainWallBmp = NULL;
 WoopsiGfx::Bitmap* BitmapServer::_brickWallBmp = NULL;
+WoopsiGfx::Bitmap* BitmapServer::_girderWallBmp = NULL;
+WoopsiGfx::Bitmap* BitmapServer::_iceWallBmp = NULL;
+WoopsiGfx::Bitmap* BitmapServer::_metalWallBmp = NULL;
+WoopsiGfx::Bitmap* BitmapServer::_rockWallBmp = NULL;
+
 WoopsiGfx::Bitmap* BitmapServer::_redDoorBmp = NULL;
 WoopsiGfx::Bitmap* BitmapServer::_greenDoorBmp = NULL;
 WoopsiGfx::Bitmap* BitmapServer::_blueDoorBmp = NULL;
 WoopsiGfx::Bitmap* BitmapServer::_magentaDoorBmp = NULL;
 WoopsiGfx::Bitmap* BitmapServer::_cyanDoorBmp = NULL;
+
 WoopsiGfx::Bitmap* BitmapServer::_soilBmp = NULL;
