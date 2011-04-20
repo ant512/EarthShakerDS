f5b1344033ff00aa558f3f1143cdbe13ec04bad6
ant512 <devnull@localhost>
2011-04-20 14:49:59 +0100

    Added comments.

--- include/screenbase.h
+++ include/screenbase.h
@@ -5,23 +5,47 @@
 
 #include "padstate.h"
 
-
+/**
+ * Base class for all separate "screens" or sections of the game, ie. the title
+ * screen, game over screen, and even the game itself.
+ */
 class ScreenBase {
 public:
+
+	/**
+	 * Constructor.
+	 * @param topGfx Pointer to the graphics object that draws on the top
+	 * physical display.
+	 * @param bottomGfx Pointer to the graphics object that draws on the bottom
+	 * physical display.
+	 */
 	ScreenBase(WoopsiGfx::Graphics* topGfx, WoopsiGfx::Graphics* bottomGfx) {
 		_topGfx = topGfx;
 		_bottomGfx = bottomGfx;
 	};
 
+	/**
+	 * Destructor.
+	 */
 	virtual ~ScreenBase() {};
 
+	/**
+	 * Iterate method runs the screen logic.  Should be called every vbl, or as
+	 * often as necessary to make the screen work correctly.
+	 * @param pad The state of the DS' keypad.
+	 */
 	virtual void iterate(PadState pad) = 0;
 
+	/**
+	 * Indicates whether or not the screen is running.  If not, calling its
+	 * iterate() method should be futile.  The program should move on to the
+	 * next section and give up on this screen.
+	 */
 	virtual bool isRunning() const = 0;
 
 protected:
-	WoopsiGfx::Graphics* _topGfx;
-	WoopsiGfx::Graphics* _bottomGfx;
+	WoopsiGfx::Graphics* _topGfx;		/**< Pointer to the graphics object for the top display. */
+	WoopsiGfx::Graphics* _bottomGfx;	/**< Pointer to the graphics object for the bottom display. */
 };
 
 #endif
