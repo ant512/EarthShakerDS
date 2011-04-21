bfd29a30acd38555f94290bbe863fdfdb11d7b25
ant512 <devnull@localhost>
2011-04-21 11:13:33 +0100

    Adjusted high score layout.

--- src/titlescreen.cpp
+++ src/titlescreen.cpp
@@ -26,22 +26,22 @@ TitleScreen::TitleScreen(WoopsiGfx::Graphics* topGfx, WoopsiGfx::Graphics* botto
 
 	// High scores
 	str.setText("High Scores");
-	bottomGfx->drawText((SCREEN_WIDTH - _font.getStringWidth(str)) / 2, 16, &_font, str, 0, str.getLength(), COLOUR_WHITE);
+	bottomGfx->drawText((SCREEN_WIDTH - _font.getStringWidth(str)) / 2, 40, &_font, str, 0, str.getLength(), COLOUR_WHITE);
 
 	str.setText("020000  M. Batty");
-	bottomGfx->drawText((SCREEN_WIDTH - _font.getStringWidth(str)) / 2, 32, &_font, str, 0, str.getLength(), COLOUR_YELLOW);
+	bottomGfx->drawText((SCREEN_WIDTH - _font.getStringWidth(str)) / 2, 56, &_font, str, 0, str.getLength(), COLOUR_YELLOW);
 
 	str.setText("016000  M. Batty");
-	bottomGfx->drawText((SCREEN_WIDTH - _font.getStringWidth(str)) / 2, 48, &_font, str, 0, str.getLength(), COLOUR_CYAN);
+	bottomGfx->drawText((SCREEN_WIDTH - _font.getStringWidth(str)) / 2, 72, &_font, str, 0, str.getLength(), COLOUR_CYAN);
 
 	str.setText("012000  M. Batty");
-	bottomGfx->drawText((SCREEN_WIDTH - _font.getStringWidth(str)) / 2, 64, &_font, str, 0, str.getLength(), COLOUR_GREEN);
+	bottomGfx->drawText((SCREEN_WIDTH - _font.getStringWidth(str)) / 2, 88, &_font, str, 0, str.getLength(), COLOUR_GREEN);
 
 	str.setText("008000  M. Batty");
-	bottomGfx->drawText((SCREEN_WIDTH - _font.getStringWidth(str)) / 2, 80, &_font, str, 0, str.getLength(), COLOUR_MAGENTA);
+	bottomGfx->drawText((SCREEN_WIDTH - _font.getStringWidth(str)) / 2, 104, &_font, str, 0, str.getLength(), COLOUR_MAGENTA);
 
 	str.setText("004000  M. Batty");
-	bottomGfx->drawText((SCREEN_WIDTH - _font.getStringWidth(str)) / 2, 96, &_font, str, 0, str.getLength(), COLOUR_RED);
+	bottomGfx->drawText((SCREEN_WIDTH - _font.getStringWidth(str)) / 2, 120, &_font, str, 0, str.getLength(), COLOUR_RED);
 
 	_scroller = new Scroller(".... Earth Shaker DS - Featuring anti-gravity, "
 							 "4D teleport, forcefields, bubbles, elixirs, "
