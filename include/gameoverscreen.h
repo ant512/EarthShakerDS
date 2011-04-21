6deb161cd0ce7bb5fe8db435765a1c8186bed972
ant512 <devnull@localhost>
2011-04-21 09:57:58 +0100

    Split gameoverscreen.h into additional .cpp file.

--- include/gameoverscreen.h
+++ include/gameoverscreen.h
@@ -5,42 +5,19 @@
 #include <graphics.h>
 #include <woopsistring.h>
 
+#include "gamefont.h"
 #include "screenbase.h"
 #include "spectrumcolours.h"
 
 class GameOverScreen : public ScreenBase {
 public:
-	GameOverScreen(WoopsiGfx::Graphics* topGfx, WoopsiGfx::Graphics* bottomGfx, s32 score, s32 level) : ScreenBase(topGfx, bottomGfx) {
-		_timer = 200;
-
-		topGfx->drawFilledRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, COLOUR_BLACK);
-		bottomGfx->drawFilledRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, COLOUR_BLACK);
-
-		WoopsiGfx::WoopsiString str = "Game Over";
-		topGfx->drawText(92, 80, &_font, str, 0, str.getLength(), COLOUR_GREEN);
-
-		str = "You scored";
-		topGfx->drawText(60, 96, &_font, str, 0, str.getLength(), COLOUR_CYAN);
-
-		str.format("%06d", score);
-		topGfx->drawText(148, 96, &_font, str, 0, str.getLength(), COLOUR_WHITE);
-
-		str = "And reached level";
-		topGfx->drawText(54, 110, &_font, str, 0, str.getLength(), COLOUR_CYAN);
-
-		str.format("%02d", level);
-		topGfx->drawText(196, 110, &_font, str, 0, str.getLength(), COLOUR_WHITE);
-	};
+	GameOverScreen(WoopsiGfx::Graphics* topGfx, WoopsiGfx::Graphics* bottomGfx, s32 score, s32 level);
 
 	~GameOverScreen() {};
 
-	void iterate(PadState pad) {
-		--_timer;
-	};
+	void iterate(PadState pad);
 
-	bool isRunning() const {
-		return _timer > 0;
-	};
+	bool isRunning() const;
 
 private:;
 	s32 _timer;
---
+++ src/gameoverscreen.cpp
@@ -0,0 +1,31 @@
+#include "gameoverscreen.h"
+
+GameOverScreen::GameOverScreen(WoopsiGfx::Graphics* topGfx, WoopsiGfx::Graphics* bottomGfx, s32 score, s32 level) : ScreenBase(topGfx, bottomGfx) {
+	_timer = 200;
+
+	topGfx->drawFilledRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, COLOUR_BLACK);
+	bottomGfx->drawFilledRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, COLOUR_BLACK);
+
+	WoopsiGfx::WoopsiString str = "Game Over";
+	topGfx->drawText(92, 80, &_font, str, 0, str.getLength(), COLOUR_GREEN);
+
+	str = "You scored";
+	topGfx->drawText(60, 96, &_font, str, 0, str.getLength(), COLOUR_CYAN);
+
+	str.format("%06d", score);
+	topGfx->drawText(148, 96, &_font, str, 0, str.getLength(), COLOUR_WHITE);
+
+	str = "And reached level";
+	topGfx->drawText(54, 110, &_font, str, 0, str.getLength(), COLOUR_CYAN);
+
+	str.format("%02d", level);
+	topGfx->drawText(196, 110, &_font, str, 0, str.getLength(), COLOUR_WHITE);
+}
+
+void GameOverScreen::iterate(PadState pad) {
+	--_timer;
+}
+
+bool GameOverScreen::isRunning() const {
+	return _timer > 0;
+}
