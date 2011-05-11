f6b03396395401672ec4d0027e8093d103740713
ant512 <devnull@localhost>
2011-05-11 15:16:26 +0100

    Scroller text scrolls into view instead of jumping into view.

--- src/scroller.cpp
+++ src/scroller.cpp
@@ -9,7 +9,7 @@ Scroller::Scroller(const WoopsiGfx::WoopsiString& text) {
 
 void Scroller::render(s32 y, WoopsiGfx::Graphics* gfx) {
 	s32 start = _offset / 8;
-	s32 length = SCREEN_WIDTH / 8;
+	s32 length = (SCREEN_WIDTH / 8) + 8;
 	s32 x = -(_offset % 8);
 
 	if (start < 0) {
