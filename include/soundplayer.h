409d5a002cad930b7495d13e27ae5ee309b904c6
ant512 <devnull@localhost>
2011-05-18 13:46:22 +0100

    Comments.

--- include/soundplayer.h
+++ include/soundplayer.h
@@ -7,8 +7,15 @@
 #include "soundbank.h"
 #include "soundbank_bin.h"
 
+/**
+ * Manages all sounds within the game.
+ */
 class SoundPlayer {
 public:
+
+	/**
+	 * Initialises all game sounds.
+	 */
 	static void init() {
 		mmInitDefaultMem((mm_addr)soundbank_bin);
 		mmLoadEffect(SFX_BARRIEREXPLODE);
@@ -30,79 +37,136 @@ public:
 		mmLoadEffect(SFX_TITLETHEME);
 	};
 
+	/**
+	 * Plays the barrier explosion sound.
+	 */
 	static void playBarrierExplode() {
 		mmEffect(SFX_BARRIEREXPLODE);
 	};
 
+	/**
+	 * Plays the barrier pushed sound.
+	 */
 	static void playBarrierPush() {
 		mmEffect(SFX_BARRIERPUSH);
 	};
 
+	/**
+	 * Plays the bean collection sound.
+	 */
 	static void playBeanCollect() {
 		mmEffect(SFX_BEANCOLLECT);
 	};
 
+	/**
+	 * Plays the block falling sound.
+	 */
 	static void playBlockFall() {
 		mmEffect(SFX_BLOCKFALL);
 	};
 
+	/**
+	 * Plays the block landing sound.
+	 */
 	static void playBlockLand() {
 		mmEffect(SFX_BLOCKLAND);
 	};
 
+	/**
+	 * Plays the boulder explosion sound.
+	 */
 	static void playBoulderExplode() {
 		mmEffect(SFX_BOULDEREXPLODE);
 	};
 
+	/**
+	 * Plays the bubble explosion sound.
+	 */
 	static void playBubbleExplode() {
 		mmEffect(SFX_BUBBLEEXPLODE);
 	};
 
+	/**
+	 * Plays the diamond collection sound.
+	 */
 	static void playDiamondCollect() {
 		mmEffect(SFX_DIAMONDCOLLECT);
 	};
 
+	/**
+	 * Plays the door opening sound.
+	 */
 	static void playDoorOpen() {
 		mmEffect(SFX_DOOROPEN);
 	};
 
+	/**
+	 * Plays the gravity inversion sound.
+	 */
 	static void playGravityInversion() {
 		mmEffectCancel(_gravityHandle);
 		_gravityHandle = mmEffect(SFX_GRAVITYINVERSION);
 	};
 
+	/**
+	 * Plays the level complete sound.
+	 */
 	static void playLevelComplete() {
 		mmEffect(SFX_LEVELCOMPLETE);
 	};
 
+	/**
+	 * Plays the game paused sound.
+	 */
 	static void playPause() {
 		mmEffect(SFX_PAUSE);
 	};
 
+	/**
+	 * Plays the player explosion sound.
+	 */
 	static void playPlayerExplode() {
 		mmEffect(SFX_PLAYEREXPLODE);
 	};
 
+	/**
+	 * Plays the soil digging sound.
+	 */
 	static void playSoilDig() {
 		mmEffect(SFX_SOILDIG);
 	};
 
+	/**
+	 * Plays the teleport collection sound.
+	 */
 	static void playTeleportCollect() {
 		mmEffect(SFX_TELEPORTCOLLECT);
 	};
 
+	/**
+	 * Plays the time ticking sound.
+	 */
 	static void playTime() {
 		mmEffect(SFX_TIME);
 	};
 
+	/**
+	 * Plays the title music.
+	 */
 	static void playTitleTheme() {
 		_titleHandle = mmEffect(SFX_TITLETHEME);
 	};
 
+	/**
+	 * Stops the title music.
+	 */
 	static void stopTitleTheme() {
 		mmEffectCancel(_titleHandle);
 	};
 
+	/**
+	 * Shuts down all game sounds.
+	 */
 	static void shutdown() {
 		mmUnloadEffect(SFX_BARRIEREXPLODE);
 		mmUnloadEffect(SFX_BARRIERPUSH);
@@ -124,11 +188,18 @@ public:
 	};
 
 private:
-	static mm_sfxhand _titleHandle;
-	static mm_sfxhand _gravityHandle;
+	static mm_sfxhand _titleHandle;		/**< Handle for the title music. */
+	static mm_sfxhand _gravityHandle;	/**< Handle for the gravity inversion sound. */
 
-	SoundPlayer();
-	~SoundPlayer();
+	/**
+	 * Constructor.
+	 */
+	SoundPlayer() { };
+
+	/**
+	 * Destructor.
+	 */
+	~SoundPlayer() { };
 };
 
 #endif
