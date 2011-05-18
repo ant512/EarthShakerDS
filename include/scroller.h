bd59562b57f062905562562ad91761e0d285af0e
ant512 <devnull@localhost>
2011-05-18 13:50:51 +0100

    Comments.

--- include/scroller.h
+++ include/scroller.h
@@ -8,19 +8,36 @@
 #include "gamefont.h"
 #include "spectrumcolours.h"
 
+/**
+ * Horizontal text scroller.
+ */
 class Scroller {
 public:
+
+	/**
+	 * Constructor.
+	 * @param text The text to display.
+	 */
 	Scroller(const WoopsiGfx::WoopsiString& text);
 
+	/**
+	 * Destructor.
+	 */
 	~Scroller() { };
 
+	/**
+	 * Renders the scroller at the specified y co-ordinate using the supplied
+	 * Graphics object.
+	 * @param y The y co-ordinate to render at.
+	 * @param gfx The Graphics object to render with.
+	 */
 	void render(s32 y, WoopsiGfx::Graphics* gfx);
 
 private:
-	s32 _offset;
-	s32 _stringWidth;
-	GameFont _font;
-	WoopsiGfx::WoopsiString _text;
+	s32 _offset;					/**< Current scroll amount. */
+	s32 _stringWidth;				/**< Width in pixels of the string. */
+	GameFont _font;					/**< Font used to render the text. */
+	WoopsiGfx::WoopsiString _text;	/**< Text being displayed. */
 };
 
 #endif
