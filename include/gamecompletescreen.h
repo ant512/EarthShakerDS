f0f9b482096eea31c72976177ce5670bdff281e1
ant512 <devnull@localhost>
2011-05-18 12:56:20 +0100

    Comments.

--- include/gamecompletescreen.h
+++ include/gamecompletescreen.h
@@ -10,19 +10,44 @@
 #include "screenbase.h"
 #include "spectrumcolours.h"
 
+/**
+ * Displayed when the player completes the game.
+ */
 class GameCompleteScreen : public ScreenBase {
 public:
+
+	/**
+	 * Constructor.
+	 * Constructor.
+	 * @param topGfx Pointer to the graphics object that draws on the top
+	 * physical display.
+	 * @param bottomGfx Pointer to the graphics object that draws on the bottom
+	 * physical display.
+	 * @param score The final score achieved by the player.
+	 */
 	GameCompleteScreen(WoopsiGfx::Graphics* topGfx, WoopsiGfx::Graphics* bottomGfx, s32 score);
 
+	/**
+	 * Destructor.
+	 */
 	~GameCompleteScreen() {};
 
+	/**
+	 * Runs the screen logic.
+	 */
 	void iterate();
 
+	/**
+	 * Indicates whether or not the screen is running.  If not, calling its
+	 * iterate() method should be futile.  The program should move on to the
+	 * next section and give up on this screen.
+	 * @return True if the screen is running; false if not.
+	 */
 	bool isRunning() const;
 
 private:;
-	s32 _timer;
-	GameFont _font;
+	s32 _timer;			/**< Length of time screen has been visible. */
+	GameFont _font;		/**< Font to render text with. */
 };
 
 #endif
