b046f150cb278ab63817016d6b42afd693df9659
ant512 <devnull@localhost>
2011-06-01 15:32:07 +0100

    Hardware class set up with SDL interaction.

--- include/soundplayer.h
+++ include/soundplayer.h
@@ -2,10 +2,15 @@
 #define _SOUND_PLAYER_H_
 
 #include <nds.h>
+
+#ifndef USING_SDL
+
 #include <maxmod9.h>
 
 #include "soundbank.h"
 #include "soundbank_bin.h"
+
+#endif
 
 /**
  * Manages all sounds within the game.
@@ -17,6 +22,9 @@ public:
 	 * Initialises all game sounds.
 	 */
 	static void init() {
+
+		#ifndef USING_SDL
+
 		mmInitDefaultMem((mm_addr)soundbank_bin);
 		mmLoadEffect(SFX_BARRIEREXPLODE);
 		mmLoadEffect(SFX_BARRIERPUSH);
@@ -40,199 +48,254 @@ public:
 		mmLoadEffect(SFX_TELEPORTCOLLECT);
 		mmLoadEffect(SFX_TIME);
 		mmLoadEffect(SFX_TITLETHEME);
+
+		#endif
 	};
 
 	/**
 	 * Plays the barrier explosion sound.
 	 */
 	static void playBarrierExplode() {
+		#ifndef USING_SDL
 		mmEffect(SFX_BARRIEREXPLODE);
+		#endif
 	};
 
 	/**
 	 * Plays the barrier pushed sound.
 	 */
 	static void playBarrierPush() {
+		#ifndef USING_SDL
 		mmEffect(SFX_BARRIERPUSH);
+		#endif
 	};
 
 	/**
 	 * Plays the bean collection sound.
 	 */
 	static void playBeanCollect() {
+		#ifndef USING_SDL
 		mmEffect(SFX_BEANCOLLECT);
+		#endif
 	};
 
 	/**
 	 * Plays the block falling sound.
 	 */
 	static void playBlockFall() {
+		#ifndef USING_SDL
 		mmEffect(SFX_BLOCKFALL);
+		#endif
 	};
 
 	/**
 	 * Plays the block landing sound.
 	 */
 	static void playBlockLand() {
+		#ifndef USING_SDL
 		mmEffect(SFX_BLOCKLAND);
+		#endif
 	};
 
 	/**
 	 * Plays the boulder explosion sound.
 	 */
 	static void playBoulderExplode() {
+		#ifndef USING_SDL
 		mmEffect(SFX_BOULDEREXPLODE);
+		#endif
 	};
 
 	/**
 	 * Plays the bubble explosion sound.
 	 */
 	static void playBubbleExplode() {
+		#ifndef USING_SDL
 		mmEffect(SFX_BUBBLEEXPLODE);
+		#endif
 	};
 
 	/**
 	 * Plays the diamond collection sound.
 	 */
 	static void playDiamondCollect() {
+		#ifndef USING_SDL
 		mmEffect(SFX_DIAMONDCOLLECT);
+		#endif
 	};
 
 	/**
 	 * Plays the door opening sound.
 	 */
 	static void playDoorOpen() {
+		#ifndef USING_SDL
 		mmEffect(SFX_DOOROPEN);
+		#endif
 	};
 
 	/**
 	 * Plays the extra life collection sound.
 	 */
 	static void playExtraLifeCollect() {
+		#ifndef USING_SDL
 		mmEffect(SFX_EXTRALIFECOLLECT);
+		#endif
 	};
 
 	/**
 	 * Plays the game over music.
 	 */
 	static void playGameOverTheme() {
+		#ifndef USING_SDL
 		_gameOverHandle = mmEffect(SFX_GAMEOVERTHEME);
+		#endif
 	};
 
 	/**
 	 * Stops the game over music.
 	 */
 	static void stopGameOverTheme() {
+		#ifndef USING_SDL
 		mmEffectCancel(_gameOverHandle);
+		#endif
 	};
 
 	/**
 	 * Plays the gravity inversion sound.
 	 */
 	static void playGravityInversion() {
+		#ifndef USING_SDL
 		mmEffectCancel(_gravityHandle);
 		_gravityHandle = mmEffect(SFX_GRAVITYINVERSION);
+		#endif
 	};
 
 	/**
 	 * Plays the level complete sound.
 	 */
 	static void playLevelComplete() {
+		#ifndef USING_SDL
 		mmEffect(SFX_LEVELCOMPLETE);
+		#endif
 	};
 
 	/**
 	 * Plays the map music.
 	 */
 	static void playMapTheme() {
+		#ifndef USING_SDL
 		mmEffectCancel(_mapHandle);
 		_mapHandle = mmEffect(SFX_MAPTHEME);
+		#endif
 	};
 
 	/**
 	 * Stops the map music.
 	 */
 	static void stopMapTheme() {
+		#ifndef USING_SDL
 		mmEffectCancel(_mapHandle);
+		#endif
 	};
 
 	/**
 	 * Plays the game paused sound.
 	 */
 	static void playPause() {
+		#ifndef USING_SDL
 		mmEffect(SFX_PAUSE);
+		#endif
 	};
 
 	/**
 	 * Plays the player explosion sound.
 	 */
 	static void playPlayerExplode() {
+		#ifndef USING_SDL
 		mmEffect(SFX_PLAYEREXPLODE);
+		#endif
 	};
 
 	/**
 	 * Plays the soil digging sound.
 	 */
 	static void playSoilDig() {
+		#ifndef USING_SDL
 		mmEffect(SFX_SOILDIG);
+		#endif
 	};
 
 	/**
 	 * Plays the soil poking sound.
 	 */
 	static void playSoilPoke() {
+		#ifndef USING_SDL
 		mmEffect(SFX_SOILPOKE);
+		#endif
 	};
 
 	/**
 	 * Plays the suicide sound.
 	 */
 	static void playSuicide() {
+		#ifndef USING_SDL
 		mmEffect(SFX_SUICIDE);
+		#endif
 	};
 
 	/**
 	 * Plays the teleport collection sound.
 	 */
 	static void playTeleportCollect() {
+		#ifndef USING_SDL
 		mmEffect(SFX_TELEPORTCOLLECT);
+		#endif
 	};
 
 	/**
 	 * Plays the time ticking sound.
 	 */
 	static void playTime() {
+		#ifndef USING_SDL
 		mmEffect(SFX_TIME);
+		#endif
 	};
 
 	/**
 	 * Plays the title music.
 	 */
 	static void playTitleTheme() {
+		#ifndef USING_SDL
 		_titleHandle = mmEffect(SFX_TITLETHEME);
+		#endif
 	};
 
 	/**
 	 * Stops the title music.
 	 */
 	static void stopTitleTheme() {
+		#ifndef USING_SDL
 		mmEffectCancel(_titleHandle);
+		#endif
 	};
 
 	/**
 	 * Stops all (long running) sounds.
 	 */
 	static void stopAll() {
+		#ifndef USING_SDL
 		mmEffectCancel(_titleHandle);
 		mmEffectCancel(_gravityHandle);
 		mmEffectCancel(_gameOverHandle);
 		mmEffectCancel(_mapHandle);
+		#endif
 	};
 
 	/**
 	 * Shuts down all game sounds.
 	 */
 	static void shutdown() {
+		#ifndef USING_SDL
 		mmUnloadEffect(SFX_BARRIEREXPLODE);
 		mmUnloadEffect(SFX_BARRIERPUSH);
 		mmUnloadEffect(SFX_BEANCOLLECT);
@@ -255,13 +318,18 @@ public:
 		mmUnloadEffect(SFX_TELEPORTCOLLECT);
 		mmUnloadEffect(SFX_TIME);
 		mmUnloadEffect(SFX_TITLETHEME);
+		#endif
 	};
 
 private:
+	#ifndef USING_SDL
+
 	static mm_sfxhand _titleHandle;		/**< Handle for the title music. */
 	static mm_sfxhand _gravityHandle;	/**< Handle for the gravity inversion sound. */
 	static mm_sfxhand _gameOverHandle;	/**< Handle for the game over music. */
 	static mm_sfxhand _mapHandle;		/**< Handle for the map music. */
+
+	#endif
 
 	/**
 	 * Constructor.
--- src/hardware.cpp
+++ src/hardware.cpp
@@ -12,6 +12,9 @@ WoopsiGfx::Graphics* Hardware::_bottomGfx = NULL;
 bool Hardware::_isMostRecentDirectionVertical = false;
 
 void Hardware::init() {
+
+	#ifndef USING_SDL
+
 	powerOn(POWER_ALL_2D);
 
 	videoSetMode( MODE_5_2D | DISPLAY_BG3_ACTIVE );
@@ -27,6 +30,31 @@ void Hardware::init() {
 	_topBuffer = new SDLFrameBuffer((u16*)BG_BMP_RAM(0), SCREEN_WIDTH, SCREEN_HEIGHT);
 	_bottomBuffer = new SDLFrameBuffer((u16*)BG_BMP_RAM_SUB(0), SCREEN_WIDTH, SCREEN_HEIGHT);
 
+	#else
+
+	initflags = SDL_INIT_VIDEO;
+	video_bpp = 0;
+	videoflags = SDL_SWSURFACE;
+
+	// Initialize the SDL library
+	if (SDL_Init(initflags) < 0) {
+		fprintf(stderr, "Couldn't initialize SDL: %s\n", SDL_GetError());
+		exit(1);
+	}
+
+	// Set video mode
+	screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT * SCREEN_COUNT, video_bpp, videoflags);
+	if (screen == NULL) {
+		fprintf(stderr, "Couldn't set %dx%dx%d video mode: %s\n", SCREEN_WIDTH, SCREEN_HEIGHT, video_bpp, SDL_GetError());
+		SDL_Quit();
+		exit(2);
+	}
+
+	_topBuffer = new WoopsiUI::SDLFrameBuffer(screen, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+	_bottomBuffer = new WoopsiUI::SDLFrameBuffer(screen, SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_HEIGHT);
+
+	#endif
+
 	_topGfx = _topBuffer->newGraphics();
 	_bottomGfx = _bottomBuffer->newGraphics();
 
@@ -41,9 +69,12 @@ void Hardware::shutdown() {
 }
 
 void Hardware::updatePadState() {
-	scanKeys();
 
 	PadState oldPad = _pad;
+
+	#ifndef USING_SDL
+
+	scanKeys();
 
 	s32 pressed = keysDown();	// buttons pressed this loop
 	s32 held = keysHeld();		// buttons currently held
@@ -62,14 +93,30 @@ void Hardware::updatePadState() {
 	_pad.start = allKeys & KEY_START;
 	_pad.select = allKeys & KEY_SELECT;
 
+	#else
+
+	 Uint8* keyState = SDL_GetKeyState(NULL);
+
+	_pad.up = keyState[SDLK_UP];
+	_pad.down = keyState[SDLK_DOWN];
+	_pad.left = keyState[SDLK_LEFT];
+	_pad.right = keyState[SDLK_RIGHT];
+	_pad.l = keyState[SDLK_a];
+	_pad.r = keyState[SDLK_s];
+	_pad.a = keyState[SDLK_z];
+	_pad.b = keyState[SDLK_x];
+	_pad.x = keyState[SDLK_c];
+	_pad.y = keyState[SDLK_v];
+	_pad.start = keyState[SDLK_d];
+	_pad.select = keyState[SDLK_f];
+
+	#endif
+
 	if ((_pad.up && !oldPad.up) || (_pad.down && !oldPad.down)) {
 		_isMostRecentDirectionVertical = true;
 	} else if ((_pad.left && !oldPad.left) || (_pad.right && !oldPad.right)) {
 		_isMostRecentDirectionVertical = false;
 	}
-
-	touchPosition touch;
-	touchRead(&touch);
 
 	if (_stylus.newPress) {
 		_stylus.held = true;
@@ -77,6 +124,8 @@ void Hardware::updatePadState() {
 	}
 
 	_stylus.released = false;
+
+	#ifndef USING_SDL
 
 	if ((allKeys & KEY_TOUCH) && (!_stylus.held)) {
 		
@@ -92,8 +141,37 @@ void Hardware::updatePadState() {
 		_stylus.held = false;
 	}
 
+	touchPosition touch;
+	touchRead(&touch);
+
 	_stylus.x = touch.px;
 	_stylus.y = touch.py;
+
+	#else
+
+	// Read mouse state
+	int mouseState;
+	int mouseX;
+	int mouseY;
+	
+	mouseState = SDL_GetMouseState(&mouseX, &mouseY);
+	
+	// Check buttons
+	if ((mouseState & SDL_BUTTON_LEFT) && (!_stylus.held)) {
+		
+		// New click
+		_stylus.newPress = true;
+		_stylus.released = false;
+
+	} else if ((!(mState & SDL_BUTTON_LEFT)) && (_stylus.held)) {
+		
+		// Release
+		_stylus.released = true;
+		_stylus.newPress = false;
+		_stylus.held = false;
+	}
+
+	#endif
 }
 
 bool Hardware::isMostRecentDirectionVertical() {
@@ -101,6 +179,17 @@ bool Hardware::isMostRecentDirectionVertical() {
 }
 
 void Hardware::waitForVBlank() {
+
+	#ifndef USING_SDL
+
 	swiWaitForVBlank();
+
+	#else
+
+	SDL_Delay(10);
+	SDL_Flip(screen);
+
+	#endif
+
 	updatePadState();
 }
