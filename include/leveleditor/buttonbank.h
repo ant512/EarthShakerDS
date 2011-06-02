d97a98761d4216892b0f56e79f51a42621dcb7a2
ant512 <devnull@localhost>
2011-06-02 10:25:56 +0100

    Added comments.

--- include/leveleditor/buttonbank.h
+++ include/leveleditor/buttonbank.h
@@ -14,28 +14,47 @@
  */
 class ButtonBank {
 public:
+
+	/**
+	 * Constructor.
+	 * @param listener Object that will react to button presses.
+	 * @param gfx Graphics object to draw to.
+	 */
 	ButtonBank(ButtonListener* listener, WoopsiGfx::Graphics* gfx) {
 		_listener = listener;
 		_gfx = gfx;
 		_clickedButton = NULL;
 	};
 
+	/**
+	 * Destructor.
+	 */
 	~ButtonBank() {
 		for (s32 i = 0; i < _buttons.size(); ++i) {
 			delete _buttons[i];
 		}
 	};
 
+	/**
+	 * Draws the button bank.
+	 */
 	void render() {
 		for (s32 i = 0; i < _buttons.size(); ++i) {
 			_buttons[i]->render(_gfx);
 		}
 	};
 
+	/**
+	 * Add a button to the bank.
+	 * @param button The button to add.
+	 */
 	void addButton(ButtonBase* button) {
 		_buttons.push_back(button);
 	};
 
+	/**
+	 * Runs all button bank logic.  Reacts to stylus presses.
+	 */
 	void iterate() {
 		const StylusState& stylus = Hardware::getStylusState();
 
@@ -46,6 +65,12 @@ public:
 		}
 	};
 
+	/**
+	 * Click the lowest button in the button stack at the specified
+	 * co-ordinates.
+	 * @param x X co-ordinate to click.
+	 * @param y Y co-ordinate to click.
+	 */
 	void click(s32 x, s32 y) {
 		for (s32 i = 0; i < _buttons.size(); ++i) {
 			if (_buttons[i]->checkCollision(x, y)) {
@@ -53,10 +78,14 @@ public:
 				_clickedButton->click();
 				_clickedButton->render(_gfx);
 				_listener->handleButtonAction(_buttons[i]);
+				return;
 			}
 		}
 	};
 
+	/**
+	 * Release the clicked button.
+	 */
 	void release() {
 		if(_clickedButton != NULL) {
 			_clickedButton->release();
@@ -67,10 +96,10 @@ public:
 	};
 
 private:
-	ButtonListener* _listener;
-	WoopsiArray<ButtonBase*> _buttons;
-	ButtonBase* _clickedButton;
-	WoopsiGfx::Graphics* _gfx;
+	ButtonListener* _listener;			/**< Object that listens for button clicks. */
+	WoopsiArray<ButtonBase*> _buttons;	/**< List of buttons. */
+	ButtonBase* _clickedButton;			/**< Pointer to the clicked button. */
+	WoopsiGfx::Graphics* _gfx;			/**< Graphics object to draw to. */
 };
 
 #endif
--- include/leveleditor/buttonbase.h
+++ include/leveleditor/buttonbase.h
@@ -5,8 +5,21 @@
 
 #include "soundplayer.h"
 
+/**
+ * Base class for all buttons.
+ */
 class ButtonBase {
 public:
+
+	/**
+	 * Constructor.
+	 * @param x X co-ordinate of the button.
+	 * @param y Y co-ordinate of the buttpn.
+	 * @param width Width of the button.
+	 * @param height Height of the button.
+	 * @param id The unique ID of the button.  Uniqueness is not enforced but
+	 * problems will arise if multiple buttons have the same ID.
+	 */
 	ButtonBase(s32 x, s32 y, s32 width, s32 height, s32 id) {
 		_x = x;
 		_y = y;
@@ -16,10 +29,23 @@ public:
 		_clicked = false;
 	};
 
+	/**
+	 * Destructor.
+	 */
 	virtual ~ButtonBase() { };
 
+	/**
+	 * Draws the button.
+	 * @param gfx The graphics object to draw to.
+	 */
 	virtual void render(WoopsiGfx::Graphics* gfx) = 0;
 
+	/**
+	 * Checks if the supplied co-ordinates intersect this button.
+	 * @param x The x co-ordinate to check.
+	 * @param y The y co-ordinate to check.
+	 * @return True if the co-ordinate intersets the button; false if not.
+	 */
 	bool checkCollision(s32 x, s32 y) {
 		return ((x >= _x) &&
 				(y >= _y) &&
@@ -27,24 +53,34 @@ public:
 				(y < _y + _height));
 	};
 
+	/**
+	 * Gets the ID of the button.
+	 * @return The ID of the button.
+	 */
 	s32 getId() const { return _id; };
 
+	/**
+	 * Click the button.
+	 */
 	void click() {
 		SoundPlayer::playSoilDig();
 		_clicked = true;
 	};
 
+	/**
+	 * Release the button.
+	 */
 	void release() {
 		_clicked = false;
 	};
 
 protected:
-	s32 _x;
-	s32 _y;
-	s32 _width;
-	s32 _height;
-	s32 _id;
-	bool _clicked;
+	s32 _x;			/**< The x co-ordinate of the button. */
+	s32 _y;			/**< The y co-ordinate of the button. */
+	s32 _width;		/**< The width of the button. */
+	s32 _height;	/**< The height of the button. */
+	s32 _id;		/**< The ID of the button. */
+	bool _clicked;	/**< Indicates whether or not the button is clicked. */
 };
 
 #endif
