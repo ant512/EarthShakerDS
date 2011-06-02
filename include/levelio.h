554afb904161b5cc22377509119138f903cf9341
ant512 <devnull@localhost>
2011-06-02 14:55:19 +0100

    Added comments.

--- include/levelio.h
+++ include/levelio.h
@@ -12,13 +12,30 @@
 class LevelIO {
 public:
 
+	/**
+	 * Save the supplied level definition to disk.  Files will be saved in
+	 * /data/earthshakerds.
+	 * @param level Level to save.
+	 */
 	static void save(LevelDefinitionBase* level);
 
+	/**
+	 * Load the specified level from disk.
+	 * @param fileName Name of the file to load.
+	 * @return The loaded level, or NULL if an error occurred.
+	 */
 	static MutableLevelDefinition* load(const WoopsiGfx::WoopsiString& fileName);
 
 private:
 
+	/**
+	 * Constructor.
+	 */
 	LevelIO() { };
+
+	/**
+	 * Destructor.
+	 */
 	~LevelIO() { };
 };
 
